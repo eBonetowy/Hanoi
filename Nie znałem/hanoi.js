d17003da1790ca2e6388f021b6ff514469adb49e
eBonetowy <kamil.ebonetowy@gmail.com>
2024-09-10 17:22:42 +0200

    Add files via upload

---
+++ Nie znałem/hanoi.js
@@ -0,0 +1,24 @@
+const getHanoiSolutions = (nDiscs) => {
+    const solutions = []
+  
+    const hanoi = (n, origin, destiny, aux) => {
+      if (n == 1) {
+        solutions.push({ disc: n, origin, destiny })
+        return;
+      }
+  
+      hanoi(n - 1, origin, aux, destiny)
+  
+      solutions.push({ disc: n, origin, destiny })
+  
+      hanoi(n - 1, aux, destiny, origin)
+    }
+  
+    hanoi(nDiscs, 0, 1, 2)
+  
+    return solutions;
+  }
+  
+  export {
+    getHanoiSolutions
+  }
